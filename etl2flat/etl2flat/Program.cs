3b83bcb9f7b2e0847cc7f62eace420d41960bfa2
raandr <42317346+raandr@users.noreply.github.com>
2018-08-12 14:45:11 +0200

    Add files via upload

---
+++ etl2flat/etl2flat/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+// https://docs.microsoft.com/en-us/dotnet/framework/wcf/feature-details/how-to-create-a-basic-rss-feed
+// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/how-to-load-xml-from-a-file
+
+namespace my_console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            XmlParser xmlParser = new XmlParser(@"World.xml");
+            xmlParser.PrintXml();
+
+            RssParser rssParser = new RssParser(@"World.xml");
+
+            System.Collections.Generic.List<string> order = 
+                new System.Collections.Generic.List<string>(new string[] 
+            {   "title", 
+                "link", 
+                "description", 
+                "laguage",
+                "copyright",
+                "lastBuildDate",
+                "image"            
+            });
+
+            TableWriter tableWriter = new TableWriter(xmlParser, order);
+
+            String s = Console.ReadLine();
+
+        }
+    }
+
+
+    class XmlParser
+    {
+        protected System.Xml.Linq.XElement fromFile;
+        public System.Collections.Generic.IEnumerable<System.Xml.Linq.XElement> xmlEnum;
+
+
+
+        public XmlParser(string filename)
+        {
+            fromFile = System.Xml.Linq.XElement.Load(filename);
+            xmlEnum = fromFile.DescendantsAndSelf();
+        }
+
+        public void PrintXml()
+        {
+            foreach (System.Xml.Linq.XElement ixE in xmlEnum)
+            {
+                if (!ixE.HasElements)
+                {
+                    Console.Write("Name: ");
+                    Console.WriteLine(ixE.Name);
+
+                    Console.Write("Value: ");
+                    Console.WriteLine(ixE.Value);
+
+                    Console.Write("Parent name: ");
+                    Console.WriteLine(ixE.Parent.Name);
+
+                    Console.WriteLine("-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-");
+                    Console.WriteLine("\r\n");
+
+                }
+            }
+
+
+        }
+    }
+
+
+    class RssParser : XmlParser
+    {
+        protected enum Rss20ChannelElement
+        {
+            title,
+            link,
+            description,
+            language,
+            copyright,
+            lastBuildDate,
+            image
+
+        };
+        int i = -1;
+        //Rss20ChannelEnum rss20Element;
+
+        //System.Collections.Generic.KeyValuePair<Rss20ChannelElement, string> rss20Element;
+        protected System.Collections.Generic.Dictionary<Rss20ChannelElement, string> rss20Element;
+        protected System.Collections.Generic.List<System.Collections.Generic.Dictionary<Rss20ChannelElement, string>> rss20Channel;
+
+        public RssParser(string filename) : base(filename)
+        {
+            rss20Element = new System.Collections.Generic.Dictionary<Rss20ChannelElement, string>();
+            rss20Channel = new System.Collections.Generic.List<System.Collections.Generic.Dictionary<Rss20ChannelElement, string>>();
+
+            foreach (System.Xml.Linq.XElement xmlElement in xmlEnum)
+            {
+                switch (xmlElement.Name.ToString())
+                {
+                    case "title":
+                        i++;
+                        this.rss20Channel.Add(new System.Collections.Generic.Dictionary<Rss20ChannelElement, string>());
+                        this.rss20Channel[i].Add(Rss20ChannelElement.title, xmlElement.Value);
+                        break;
+
+                    case "link":
+                        this.rss20Channel[i].Add(Rss20ChannelElement.link, xmlElement.Value);
+                        break;
+
+                    case "description":
+                        this.rss20Channel[i].Add(Rss20ChannelElement.description, xmlElement.Value);
+                        break;
+
+                    case "language":
+                        this.rss20Channel[i].Add(Rss20ChannelElement.language, xmlElement.Value);
+                        break;
+
+                    case "copyright":
+                        this.rss20Channel[i].Add(Rss20ChannelElement.copyright, xmlElement.Value);
+                        break;
+
+                    case "lastBuildDate":
+                        this.rss20Channel[i].Add(Rss20ChannelElement.lastBuildDate, xmlElement.Value);
+                        break;
+
+                    case "image":
+                        this.rss20Channel[i].Add(Rss20ChannelElement.image, xmlElement.Value);
+                        break;
+
+                    default:
+
+                        break;
+                        
+                }
+
+
+            }
+        }
+
+
+    }
+
+
+    class TableWriter
+    {
+        private char columnDelimiter;
+        //private System.Collections.Generic.List<string> row;
+        string[] row;
+        private string line;
+        int index = -1;
+
+        public TableWriter(XmlParser xmlP, System.Collections.Generic.List<string> fieldOrder)
+        {
+            int length;
+            int i = 0;
+            length = fieldOrder.Count;
+            row = new string[length];
+
+            foreach (System.Xml.Linq.XElement xE in xmlP.xmlEnum)
+            {
+                index = fieldOrder.IndexOf(xE.Name.ToString());
+                if (index == -1)
+                    continue;
+                if (i == index)
+                {
+                    RowToLine();
+
+                }
+                row[index] = xE.Value;
+                i++;
+
+            }
+
+
+        }
+
+    }
+
+}
