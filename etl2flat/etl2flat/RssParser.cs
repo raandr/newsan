67c8ee08c967a0f710cb5d6c007fc4fca96b8123
anatoly <Anatoly.Rubekin@gmail.com>
2018-09-02 16:32:41 +0200

    commit

--- etl2flat/etl2flat/RssParser.cs
+++ etl2flat/etl2flat/RssParser.cs
@@ -6,16 +6,22 @@ namespace NewsAn
         protected enum Rss20ChannelElement
         {
             title,
+            guid,
+            pubDate,
+            url,
+            item,
             link,
             description,
             language,
             copyright,
+            category,
             lastBuildDate,
             image
-
         };
-        int i = -1;
-        //Rss20ChannelEnum rss20Element;
+
+        int i = -1; //See "title"
+
+        //Rss20ChannelEnum rss20Element;title,
 
         //System.Collections.Generic.KeyValuePair<Rss20ChannelElement, string> rss20Element;
         protected System.Collections.Generic.Dictionary<Rss20ChannelElement, string> rss20Element;
@@ -25,47 +31,119 @@ namespace NewsAn
         {
             rss20Element = new System.Collections.Generic.Dictionary<Rss20ChannelElement, string>();
             rss20Channel = new System.Collections.Generic.List<System.Collections.Generic.Dictionary<Rss20ChannelElement, string>>();
+            string xmlElementName;
 
             foreach (System.Xml.Linq.XElement xmlElement in xmlEnum)
             {
-                switch (xmlElement.Name.ToString())
+                try 
                 {
-                    case "title":
-                        i++;
-                        this.rss20Channel.Add(new System.Collections.Generic.Dictionary<Rss20ChannelElement, string>());
-                        this.rss20Channel[i].Add(Rss20ChannelElement.title, xmlElement.Value);
-                        break;
+                    xmlElementName = xmlElement.Name.ToString();
+                    switch (xmlElementName)
+                    {
+                        // Every new title element defines a new news item 
+                        case "title":
+                            i++;
+                            this.rss20Channel.Add(new System.Collections.Generic.Dictionary<Rss20ChannelElement, string>());
+                            this.rss20Channel[i].Add(Rss20ChannelElement.title, xmlElement.Value);
+                            break;
 
-                    case "link":
-                        this.rss20Channel[i].Add(Rss20ChannelElement.link, xmlElement.Value);
-                        break;
+                            /* ****************************************************************************************** */
 
-                    case "description":
-                        this.rss20Channel[i].Add(Rss20ChannelElement.description, xmlElement.Value);
-                        break;
+                        case "link":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.link, xmlElement.Value);
+                            break;
 
-                    case "language":
-                        this.rss20Channel[i].Add(Rss20ChannelElement.language, xmlElement.Value);
-                        break;
+                        case "description":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.description, xmlElement.Value);
+                            break;
 
-                    case "copyright":
-                        this.rss20Channel[i].Add(Rss20ChannelElement.copyright, xmlElement.Value);
-                        break;
+                        case "language":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.language, xmlElement.Value);
+                            break;
 
-                    case "lastBuildDate":
-                        this.rss20Channel[i].Add(Rss20ChannelElement.lastBuildDate, xmlElement.Value);
-                        break;
+                        case "copyright":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.copyright, xmlElement.Value);
+                            break;
 
-                    case "image":
-                        this.rss20Channel[i].Add(Rss20ChannelElement.image, xmlElement.Value);
-                        break;
+                        case "lastBuildDate":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.lastBuildDate, xmlElement.Value);
+                            break;
 
-                    default:
+                        case "image":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.image, xmlElement.Value);
+                            break;
 
-                        break;
+                        case "url":
+                            // Do
+                            break;
+
+                        case "item":
+                            // Need to do foreach item
+                            break;
+
+                        case "guid":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.guid, xmlElement.Value);
+                            break;
+
+                        case "pubDate":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.pubDate, xmlElement.Value);
+                            break;
+
+                        case "category":
+                            // Do nothing
+                            break;
+
+
+
+
+
+
+
+                            /*
+                             * {http://search.yahoo.com/mrss/}content
+                             * {http://search.yahoo.com/mrss/}description
+                             * {http://search.yahoo.com/mrss/}credit
+                             * {http://purl.org/dc/elements/1.1/}creator
+                             * 
+                             *
+                             */
+
+
+
+
+                        case "{http://www.w3.org/2005/Atom}link":
+                            this.rss20Channel[i].Add(Rss20ChannelElement.link, xmlElement.Value);
+                            break;
+
+
+                        case "rss":
+                            // Do nothing
+                            break;
+
+                        case "channel":
+                            // Do nothing
+                            break;
+
+
+
+
+
+                        default:
+                            throw new ArgumentOutOfRangeException(xmlElementName);
+                            //break;
+
+                    }
+
+
 
                 }
 
+
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.ParamName);
+
+                }
 
             }
         }
