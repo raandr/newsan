6b092cc806383c691589e550719961f74064ff53
eternaleap <integralzsh@gmail.com>
2019-10-15 23:39:52 +0200

    added parsing

--- interfaxParser/src/main/java/InterfaxNewsItem.java
+++ interfaxParser/src/main/java/InterfaxNewsItem.java
@@ -1,9 +1,13 @@
 import java.util.Date;
 
 public final class InterfaxNewsItem {
+    public String id;
+
     public Date date;
 
     public String source;
+
+    public String link;
 
     public String text;
 }
--- interfaxParser/src/main/java/Main.java
+++ interfaxParser/src/main/java/Main.java
@@ -1,38 +1,47 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        /*
-        WebDriver webDriver = new PhantomJSDriver();
-        webDriver.get("http://e-disclosure.ru/poisk-po-soobshheniyam");
-        Document doc = Jsoup.parse(webDriver.getPageSource());
-        Element newsHeadlines = doc.selectFirst("#searchResults");
-        */
-        //System.setProperty("phantomjs.binary.path", "libs/phantomjs"); // path to bin file. NOTE: platform dependent
-        WebDriver ghostDriver = new PhantomJSDriver();
-        try {
-            ghostDriver.get("http://e-disclosure.ru/");
-            Document doc = Jsoup.parse(ghostDriver.getPageSource());
-            Elements newsHeadlines = doc.select("td:contains(09.10.2019)");
+    private static String _siteUrl = "http://e-disclosure.ru";
 
-            for (Element item : newsHeadlines) {
-                String  data = item.data();
-            }
-            newsHeadlines = doc.select("td:contains(\"08.10.2019\")");
-        } finally {
-            ghostDriver.quit();
+    public static void main(String[] args) throws IOException, ParseException {
+        String dateStr = "15.10.2019";
+
+        for (Element item : getLinks(dateStr)) {
+            Node dataNode = item.parentNode().childNode(3);
+
+            InterfaxNewsItem newsItem = new InterfaxNewsItem();
+            DateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy", Locale.ENGLISH);
+            newsItem.source = ((TextNode)dataNode.childNodes().get(1).childNode(0)).text();
+            newsItem.date = dateFormat.parse(dateStr);
+
+            String linkNode = _siteUrl + dataNode.childNode(5).attr("href");
+
+            newsItem.text = getNewsText(linkNode);
+            newsItem.text = getNewsText(linkNode);
         }
-
     }
 
-    private static
+    private static Elements getLinks(String dateString) throws IOException {
+        Document doc = Jsoup.connect(_siteUrl).get();
+        Elements newsHeadlines = doc.select("td:contains(" + dateString +")");
+        return newsHeadlines;
+    }
+
+    private static String getNewsText(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        return doc.text();
+    }
+
 }
